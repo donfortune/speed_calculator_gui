1e8cf68630dce273cd60a75a9a7d263f2b3754f1
donfortune <donfortunet.df@gmail.com>
2023-06-14 20:30:21 +0100

    configured widgets

--- main.py
+++ main.py
@@ -11,28 +11,40 @@ class SpeedCalculator(QWidget):  #inherit from qwidget, because it creates windo
 
         # creates widgets
         distance_label = QLabel('Distance')
-        distance_line_edit = QLineEdit()
+        self.distance_line_edit = QLineEdit()
 
-        combo = QComboBox()
-        combo.addItems(['Meters(Km)', 'Miles'])
+        self.combo = QComboBox()
+        self.combo.addItems(['Meters(Km)', 'Miles'])
 
         time_hours_label = QLabel('Time(hours)')
-        time_hours_line_edit = QLineEdit()
+        self.time_hours_line_edit = QLineEdit()
 
         calculate_button = QPushButton('Calculate')
+        calculate_button.clicked.connect(self.calculate_speed)
         self.output_label = QLabel('')
 
         grid.addWidget(distance_label, 0, 0)
-        grid.addWidget(distance_line_edit, 0, 1)
+        grid.addWidget(self.distance_line_edit, 0, 1)
         grid.addWidget(time_hours_label, 1, 0)
-        grid.addWidget(time_hours_line_edit, 1, 1)
+        grid.addWidget(self.time_hours_line_edit, 1, 1)
         grid.addWidget(calculate_button, 3, 0, 1, 2, )
-        grid.addWidget(combo,  2, 0, 1, 2)
+        grid.addWidget(self.combo,  2, 0, 1, 2)
+        grid.addWidget(self.output_label, 4, 0, 1, 2)
 
         self.setLayout(grid)
 
+    def calculate_speed(self):
+        distance = float(self.distance_line_edit.text())
+        time = float(self.time_hours_line_edit.text())
+        if self.combo == 'Meters(Km)':
+            result = distance / time
+            self.output_label.setText(f'Speed: {result} m/s')
+        elif self.combo == 'Miles':
+            result = distance / (time * 0.621371)
+            self.output_label.setText(f'Speed: {result} mph')
+
 
 app = QApplication(sys.argv)
-age_calculator = SpeedCalculator()
-age_calculator.show()
+speed_calculator = SpeedCalculator()
+speed_calculator.show()
 sys.exit(app.exec())
